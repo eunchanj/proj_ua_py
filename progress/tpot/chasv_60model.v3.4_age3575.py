14aa1422020bcf33f8ed02d3cad764b9124cbc32
eunchanj <50732889+eunchanj@users.noreply.github.com>
2022-02-24 15:46:15 +0900

    Add files via upload

---
+++ progress/tpot/chasv_60model.v3.4_age3575.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=0)
+
+# Average CV score on the training set was: 0.8464466360649249
+exported_pipeline = XGBClassifier(colsample_bytree=1.0000000000000002, gamma=1.0, learning_rate=0.01, max_depth=2, min_child_weight=20, n_estimators=500, n_jobs=1, objective="binary:logistic", reg_alpha=0, reg_lambda=1, scale_pos_weight=16, subsample=0.4, tree_method="gpu_hist", verbosity=0)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 0)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
